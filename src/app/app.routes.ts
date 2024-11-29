07e7e0b83f27e3dbc87c10471de398482258516e
drusak21 <drusak21@student.foi.hr>
2024-11-29 09:17:28 +0100

    Added routes for navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AssetsComponent } from './assets/assets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'assets', component: AssetsComponent},
+];
