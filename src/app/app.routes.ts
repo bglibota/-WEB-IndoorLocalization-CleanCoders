1a36470b32feaa665a5f9da0fd3b5ae3451530ca
drusak21 <drusak21@student.foi.hr>
2024-11-29 09:31:40 +0100

    Added routes for facilites and reports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { AssetsComponent } from './assets/assets.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
+import { ReportsComponent } from './reports/reports.component';
 
 export const routes: Routes = [
     {path: 'assets', component: AssetsComponent},
+    {path: 'facilities', component: FacilitiesComponent},
+    {path: 'reports', component: ReportsComponent},
 ];
