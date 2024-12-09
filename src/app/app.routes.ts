d7cd410f9996389ea2f5633e0686aeec262beb0e
drusak21 <drusak21@student.foi.hr>
2024-12-09 18:08:22 +0100

    add routes for login and registration page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,13 @@ import { Routes } from '@angular/router';
 import { AssetsComponent } from './Pages/assets/assets.component';
 import { FacilitiesComponent } from './Pages/facilities/facilities.component';
 import { ReportsComponent } from './Pages/reports/reports.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },  
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     {path: 'assets', component: AssetsComponent},
     {path: 'facilities', component: FacilitiesComponent},
     {path: 'reports', component: ReportsComponent},
