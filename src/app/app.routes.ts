e994ec7b827ea0c5417518922fc960c601160be4
drusak21 <drusak21@student.foi.hr>
2024-12-20 10:10:54 +0100

    logic for auth guard and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { FacilitiesComponent } from './Pages/facilities/facilities.component';
 import { ReportsComponent } from './Pages/reports/reports.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegisterComponent } from './Pages/register/register.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },  
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    {path: 'assets', component: AssetsComponent},
-    {path: 'facilities', component: FacilitiesComponent},
-    {path: 'reports', component: ReportsComponent},
+    {path: 'assets', component: AssetsComponent, canActivate: [authGuard]},
+    {path: 'facilities', component: FacilitiesComponent, canActivate: [authGuard]},
+    {path: 'reports', component: ReportsComponent, canActivate: [authGuard]},
 ];
