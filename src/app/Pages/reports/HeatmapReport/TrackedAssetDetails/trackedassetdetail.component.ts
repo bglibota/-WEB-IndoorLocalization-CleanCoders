601241063875323a033c0f126456b60db67e8f1f
Toni Papić <tpapic21@foi.hr>
2025-01-17 16:18:33 +0100

    TrackedAssetDetail - group assets and show heatmap and coordinates history foreach

--- src/app/Pages/reports/HeatmapReport/TrackedAssetDetails/trackedassetdetail.component.ts
+++ src/app/Pages/reports/HeatmapReport/TrackedAssetDetails/trackedassetdetail.component.ts
@@ -1,39 +1,56 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Console } from 'console';
+import { ReportGeneratorService } from '../../../../services/reportGenerator.service';
+import { AssetPositionHistoryGET } from '../../../../models/AssetPositionHistory';
+import { CommonModule } from '@angular/common';
 var route:Router;
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './trackedassetdetail.component.html',
   styleUrl: './trackedassetdetail.component.scss'
 })
 export class TrackedAssetDetailComponent {
 reportType: any;
 
-  startDate: Date = new Date();
-  endDate: Date = new Date();
-  startTime: Date = new Date();
-  endTime: Date = new Date();
+  startDate: String = '';
+  endDate: String = '';
+  startTime: String = '';
+  endTime: String = '';
+  AssetPositionHistorylist: AssetPositionHistoryGET[] = [];
+assetsListOnly: string[] = [];
+assetFilteredData: AssetPositionHistoryGET[] = [];
 
-  constructor(private route:ActivatedRoute) { }
+
+  constructor(private route:ActivatedRoute, private reportGenerator: ReportGeneratorService) {
+
+   }
 
   ngOnInit() {
+
     this.route.params.subscribe(params => {
-      this.startDate = new Date(params['startDate']);
-      this.endDate = new Date(params['endDate']);
-      this.startTime = new Date(params['startTime']);
-      this.endTime = new Date(params['endTime']);
+      this.startDate = params['startDate'];
+      this.endDate = params['endDate'];
+      this.startTime = params['startTime'];
+      this.endTime = params['endTime'];
     });
 
+    //await this.reportGenerator.getHeatmapReportData( this.startDate, this.endDate, this.startTime, this.endTime);
+    this.AssetPositionHistorylist=this.reportGenerator.heatmapReportDataList;
+    this.assetsListOnly = Array.from(
+      new Set(this.AssetPositionHistorylist.map(item => item.assetName))
+  );
+  
     
-    console.log(this.startDate, this.endDate, this.startTime, this.endTime);
+    
    
   }
 
-  generateReport() {
-    throw new Error('Method not implemented.');
-    }
+
+filterData(assetName: string) {
+  this.assetFilteredData = this.AssetPositionHistorylist.filter(item => item.assetName === assetName);
+}
+  
 }
