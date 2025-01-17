e4feccb12824579de30e2b066f692dca24d0b339
Toni Papić <tpapic21@foi.hr>
2025-01-17 16:17:38 +0100

    Add Floormap filter

--- src/app/Pages/reports/HeatmapReport/heatmapreport.component.ts
+++ src/app/Pages/reports/HeatmapReport/heatmapreport.component.ts
@@ -3,105 +3,112 @@ import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ReportGeneratorService } from '../../../services/reportGenerator.service';
 import { AssetPositionHistoryGET } from '../../../models/AssetPositionHistory';
+import { Router } from '@angular/router';
+import { Heatmap } from './Components/Heatmap';
+
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './heatmapreport.component.html',
   styleUrls: ['./heatmapreport.component.scss']
 })
 export class HeatmapReportComponent implements OnInit {
+
   @ViewChild('heatmapCanvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
 
   reportType: any;
-  startDate: Date = new Date();
-  endDate: Date = new Date();
-  startTime: Date = new Date();
-  endTime: Date = new Date();
+  startDate: String = '';
+  endDate: String = '';
+  startTime: String = '';
+  endTime: String = '';
   AssetPositionHistorylist: AssetPositionHistoryGET[] = [];
-
+  AssetPositionHistorylistOrig: AssetPositionHistoryGET[] = [];
+  selectedFloorMap: any;
  
 
-  constructor(private route: ActivatedRoute, private reportGenerator: ReportGeneratorService) { }
+  constructor(private route: ActivatedRoute, private reportGenerator: ReportGeneratorService, private router:Router) { }
 
   async ngOnInit(): Promise<void> {
     this.getURLParams();
     await this.loadData();
-    this.drawHeatmap();
-
+    this.onFloorMapChange(this.selectedFloorMap);
   }
 
+
+  private drawHeatmap() {
+    const heatmap = new Heatmap(this.AssetPositionHistorylist, this.canvas);
+    heatmap.drawHeatmap();
+  }
+
+  getNumberOfTrackedAssets(): string {
+    const uniqueAssetNames = new Set();
+  
+    this.AssetPositionHistorylist.forEach(element => {
+      const { assetName } = element;
+      uniqueAssetNames.add(assetName);
+    });
+  
+    return uniqueAssetNames.size.toString();
+  }
 
 
   private getURLParams() {
     this.route.params.subscribe(params => {
-      this.startDate = new Date(params['startDate']);
-      this.endDate = new Date(params['endDate']);
-      this.startTime = new Date(params['startTime']);
-      this.endTime = new Date(params['endTime']);
+      this.startDate = params['startDate'].toString();
+      this.endDate = params['endDate'].toString();
+      this.startTime = params['startTime'].toString();
+      this.endTime = params['endTime'].toString();
     });
   }
 
+  onFloorMapChange(selectedFloor: string): void {
+    this.selectedFloorMap = selectedFloor;
+
+    this.FilterAssetsByFloor(selectedFloor);
+    this.drawHeatmap();
+    this.UpdateReportGeneratorService(this.AssetPositionHistorylist);
+}
+
   private async loadData() {
     try {
-      const response = await this.reportGenerator.getHeatmapReportData('2025-01-01', '2025-01-14', '08:00', '17:00');
+      const response = await this.reportGenerator.getHeatmapReportData(this.startDate,this.endDate ,this.startTime ,this.endTime );
       console.log(response);
       if (response != null) {
+        this.AssetPositionHistorylistOrig = response;
         this.AssetPositionHistorylist = response;
+        this.UpdateReportGeneratorService(this.AssetPositionHistorylist);
+
+        this.selectedFloorMap = this.getFloorMaps()[0];
       }
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   }
 
-  drawHeatmap(): void {
-    const canvas = this.canvas.nativeElement;
-    const context = canvas.getContext('2d');
-    if (!context) return;
+    UpdateReportGeneratorService(list: AssetPositionHistoryGET[]) {
+      this.reportGenerator.heatmapReportDataList = list;
 
-    canvas.width = 500;
-    canvas.height = 500;
+    }
 
-    const densityMap = this.calculateDensity();
-
-    densityMap.forEach(([x, y, density]) => {
-      const color = this.getColorForDensity(density);
-      context.beginPath();
-      context.arc(x, y, 10 * density, 0, Math.PI * 2);
-      context.fillStyle = color;
-      context.fill();
-      context.closePath();
-    });
+    getFloorMaps() {
+      const floorMaps = new Set(
+          this.AssetPositionHistorylistOrig.map(element => element.floorMapName)
+      );
+      return Array.from(floorMaps);
   }
 
-  calculateDensity(): [number, number, number][] {
-    const densityMap: Map<string, { x: number; y: number; count: number }> = new Map();
-
-    this.AssetPositionHistorylist.forEach(({ x, y }) => {
-      const key = `${x}-${y}`;
-      if (!densityMap.has(key)) {
-        densityMap.set(key, {  x, y, count: 1 });
-      } else {
-        densityMap.get(key)!.count++;
-      }
-    });
-
-    return Array.from(densityMap.values()).map(({ x, y, count }) => [x, y, count]);
+  viewAssetDetails() {
+    this.router.navigate(['/trackedassetdetails/', this.startDate, this.endDate, this.startTime, this.endTime]);
   }
 
-  getColorForDensity(density: number): string {
-    const maxDensity = 20;
-    const normalizedDensity = Math.min(density / maxDensity, 1);
-
-
-    const red = Math.min(255, Math.floor(255 * normalizedDensity));
-    const green = Math.max(0, Math.floor(255 * (1 - normalizedDensity)));
-
-    return `rgba(${red}, ${green}, 0, 0.7)`;
-  }
-  generateReport() {
-    throw new Error('Method not implemented.');
+  FilterAssetsByFloor(selectedFloor: string) {
+      
+          this.AssetPositionHistorylist = this.AssetPositionHistorylistOrig.filter(
+              element => element.floorMapName === selectedFloor);
   }
 }
 
