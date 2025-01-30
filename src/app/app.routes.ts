bc7e2f887ed3be38c6e0369b4052734372f108c9
Toni Papić <tpapic21@foi.hr>
2025-01-30 18:16:14 +0100

    Add dropdown menu for reports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,16 @@
 import { Routes } from '@angular/router';
 import { AssetsComponent } from './Pages/assets/assets.component';
 import { FacilitiesComponent } from './Pages/facilities/facilities.component';
-import { ReportsComponent } from './Pages/reports/reports.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegisterComponent } from './Pages/register/register.component';
 import { authGuard } from './guards/auth.guard';
 import { HeatmapReportComponent } from './Pages/reports/HeatmapReport/heatmapreport.component';
-import { TrackedAssetDetailComponent } from './Pages/reports/HeatmapReport/TrackedAssetDetails/trackedassetdetail.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },  
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'heatmapreport/:startDate/:endDate/:startTime/:endTime', component: HeatmapReportComponent  },
-    { path: 'trackedassetdetails/:startDate/:endDate/:startTime/:endTime', component: TrackedAssetDetailComponent  },
+    { path: 'heatmapreport', component: HeatmapReportComponent  },
     {path: 'assets', component: AssetsComponent, canActivate: [authGuard]},
     {path: 'facilities', component: FacilitiesComponent, canActivate: [authGuard]},
-    {path: 'reports', component: ReportsComponent, canActivate: [authGuard]},
 ];
