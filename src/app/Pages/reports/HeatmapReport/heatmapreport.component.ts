f8f6f78ca8054ee530ada6b81569588ba2da53f9
Toni Papić <tpapic21@foi.hr>
2025-02-01 20:45:39 +0100

    Add UI components to the heatmap report

--- src/app/Pages/reports/HeatmapReport/heatmapreport.component.ts
+++ src/app/Pages/reports/HeatmapReport/heatmapreport.component.ts
@@ -1,20 +1,19 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import { ReportGeneratorService } from '../../../services/reportGenerator.service';
 import { AssetPositionHistoryGET } from '../../../models/AssetPositionHistory';
-import { Router } from '@angular/router';
 import { Heatmap } from './Components/Heatmap';
-
+import { CalendarModule } from 'primeng/calendar';
 import { CommonModule } from '@angular/common';
 import { FloormapService } from '../../../services/floormap.service';
 import { FloorMap } from '../../../models/FloorMap';
-
-
+import { FloatLabelModule } from 'primeng/floatlabel';
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, CalendarModule,FloatLabelModule
+
+  ],
   templateUrl: './heatmapreport.component.html',
   styleUrls: ['./heatmapreport.component.scss']
 })
@@ -31,9 +30,9 @@ export class HeatmapReportComponent implements OnInit {
   assetFilteredData: AssetPositionHistoryGET[] = [];
   AssetPositionHistorylist: AssetPositionHistoryGET[] = [];
   AssetPositionHistorylistOrig: AssetPositionHistoryGET[] = [];
-  floorMapList:FloorMap[]=[];
-  selectedFloorMap: FloorMap = { Id: 0, Name: '' , Image: ''};
- 
+  floorMapList: FloorMap[] = [];
+  selectedFloorMap: FloorMap = { Id: 0, Name: '', Image: '' };
+
 
   constructor(private reportGenerator: ReportGeneratorService, private floorMapService: FloormapService) {
 
@@ -41,26 +40,26 @@ export class HeatmapReportComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     await this.getFloorMaps();
-   
+
   }
 
-  drawHeatmapForAsset(canvasId:string) {
+  drawHeatmapForAsset(canvasId: string) {
     const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
-    console.log("canvas",canvas);
+    console.log("canvas", canvas);
     const heatmap = new Heatmap(this.assetFilteredData, new ElementRef(canvas));
-    console.log("heatmap",heatmap);
-    
+    console.log("heatmap", heatmap);
+
     heatmap.drawHeatmap();
   }
 
   getNumberOfTrackedAssets(): string {
     const uniqueAssetNames = new Set();
-  
+
     this.AssetPositionHistorylist.forEach(element => {
       const { assetName } = element;
       uniqueAssetNames.add(assetName);
     });
-  
+
     return uniqueAssetNames.size.toString();
   }
 
@@ -70,12 +69,12 @@ export class HeatmapReportComponent implements OnInit {
 
     this.FilterAssetsByFloor(this.selectedFloorMap);
     this.loadAssets();
-}
+  }
 
   private async loadData() {
     try {
-      const response = await this.reportGenerator.getHeatmapReportData(this.startDate.toString(),this.endDate.toString() ,this.startTime.toString(),this.endTime.toString());
-      console.log(response);
+      console.log("startDate", this.startTime.toTimeString().split(' ')[0]);
+      const response = await this.reportGenerator.getHeatmapReportData(this.startDate.toISOString().split('T')[0], this.endDate!!.toISOString().split('T')[0], this.startTime.toTimeString().split(' ')[0], this.endTime.toTimeString().split(' ')[0]);
       if (response != null) {
         this.AssetPositionHistorylistOrig = response;
         this.AssetPositionHistorylist = response;
@@ -87,43 +86,43 @@ export class HeatmapReportComponent implements OnInit {
     }
   }
 
-   
 
-    async getFloorMaps() {
-      const floorMaps = await this.floorMapService.getAllFloormap();
-      console.log("floorMaps",Array.from(floorMaps));
-     this.floorMapList= Array.from(floorMaps);
+
+  async getFloorMaps() {
+    const floorMaps = await this.floorMapService.getAllFloormap();
+    console.log("floorMaps", Array.from(floorMaps));
+    this.floorMapList = Array.from(floorMaps);
   }
 
- 
-
-FilterAssetsByFloor(selectedFloor: FloorMap) {
-    
-        this.AssetPositionHistorylist = this.AssetPositionHistorylistOrig.filter(
-            element => element.floorMapId === selectedFloor.Id!!);
-}
 
 
-filterAccordionAssetData(assetName: string, index: number) {
-  this.assetFilteredData = this.AssetPositionHistorylist.filter(item => item.assetName === assetName);
-  
-}
+  FilterAssetsByFloor(selectedFloor: FloorMap) {
 
-loadAssets() {
-
-  this.assetsListOnly = Array.from(
-    new Set(this.AssetPositionHistorylist.map(item => item.assetName))
-);
-  console.log("AssetPositionHistorylist",this.assetsListOnly);
-}
+    this.AssetPositionHistorylist = this.AssetPositionHistorylistOrig.filter(
+      element => element.floorMapId === selectedFloor.Id!!);
+  }
 
 
-async generateReport() {
-  await this.loadData();
-  this.onFloorMapChange(this.selectedFloorMap); 
-  this.loadAssets();
-  
-}
+  filterAccordionAssetData(assetName: string, index: number) {
+    this.assetFilteredData = this.AssetPositionHistorylist.filter(item => item.assetName === assetName);
+
+  }
+
+  loadAssets() {
+
+    this.assetsListOnly = Array.from(
+      new Set(this.AssetPositionHistorylist.map(item => item.assetName))
+    );
+    console.log("AssetPositionHistorylist", this.assetsListOnly);
+  }
+
+
+  async generateReport() {
+    await this.loadData();
+    this.onFloorMapChange(this.selectedFloorMap);
+    this.loadAssets();
+
+  }
 
 
 
