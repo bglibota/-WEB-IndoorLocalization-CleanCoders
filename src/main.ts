76abdc4009c312caeb5c32405d45edd8f36db9e8
Toni Papić <tpapic21@foi.hr>
2025-02-01 20:44:59 +0100

    Apply default theme for UI library

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication,BrowserModule } from '@angular/platform-browser';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    importProvidersFrom(BrowserAnimationsModule,BrowserModule),
     provideRouter(routes, withComponentInputBinding()), 
     provideHttpClient(), 
   ],
